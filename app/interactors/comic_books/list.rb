6f1c35c5329e359e718e706eb1ec041407a731ae
Leandro Camargo <leandroico@gmail.com>
2020-12-25 19:19:09 -0300

    Create interactor to be used in comics listing endpoint
    This interactor fetches comics from the Marvel API and turn the entries
    into `ComicBook` models, in order to be easily serializable and hence
    easily presented in internal JSON based endpoint. This yet to be done
    endpoint will be used to present the comics through 'ajax' calls.

---
+++ app/interactors/comic_books/list.rb
@@ -0,0 +1,49 @@
+module ComicBooks
+  class List
+    include Interactor
+
+    def call
+      context.resources = comics.map { |comic| ComicBook.new(comic) }
+      context.pagination_data = pagination_data
+    end
+
+    private
+
+    def page
+      context.page || 1
+    end
+
+    def request_url
+      URI.decode(
+        Rails
+          .application
+          .routes.url_helpers
+          .comics_path(page: '%s')
+      )
+    end
+
+    def pagination_data
+      paginator.to_json_api_params
+    end
+
+    def paginator
+      @paginator ||= Pagination.new(
+        page: page,
+        url_template: request_url,
+        total: total_entries
+      )
+    end
+
+    def total_entries
+      comics_from_api.dig('data', 'total')
+    end
+
+    def comics
+      comics_from_api.dig('data', 'results')
+    end
+
+    def comics_from_api
+      @comics_from_api ||= MarvelApi.new.comics(page: page)
+    end
+  end
+end
